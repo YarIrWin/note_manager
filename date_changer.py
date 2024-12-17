0363f313754b61b1cc0e649a60e81780b7bccdbb
YarIrWin <yar-vinter@yandex.ru>
2024-12-17 13:59:11 +0300

    Delete date_changer.py

--- date_changer.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-created_date = input("введите дату создания заметки ")
-issue_date = input("введите дату окончания срока заметки ")
-created_date_obj = datetime.strptime(created_date, "%d/%m/%Y")
-issue_date_obj = datetime.strptime(issue_date, "%d/%m/%Y")
-print(created_date_obj.day, created_date_obj.month)
-print(issue_date_obj.day,issue_date_obj.month)
