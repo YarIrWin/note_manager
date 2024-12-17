385aef72666599d95744050bc9e3361f350a6c63
YarIrWin <yar-vinter@yandex.ru>
2024-12-17 14:06:20 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+created_date = input("введите дату создания заметки ")
+issue_date = input("введите дату окончания срока заметки ")
+title = input("введите заголовок заметки ")
+created_date_obj = datetime.strptime(created_date, "%d/%m/%Y")
+issue_date_obj = datetime.strptime(issue_date, "%d/%m/%Y")
+
+print(created_date_obj.day, created_date_obj.month)
+print(issue_date_obj.day,issue_date_obj.month)
+print("ваша заметка " + title)
